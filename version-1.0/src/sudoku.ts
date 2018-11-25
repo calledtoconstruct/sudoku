0d1a8d362a1e3d536eacd5911a096df201cd7fac
Joseph Woolley <joe@calledtoconstruct.net>
2018-11-24 23:03:45 -0500

    First test pass

---
+++ version-1.0/src/sudoku.ts
@@ -0,0 +1,6 @@
+
+export class Engine {
+    static evaluate(board: Array<number>, x: number, y: number) {
+        return 9;
+    }
+}
--- version-1.0/test/sudoku.spec.ts
+++ version-1.0/test/sudoku.spec.ts
@@ -1,6 +1,30 @@
+import { Engine } from '../src/sudoku';
 
-describe('sample', () => {
-    it('test', () => {
-        expect(true).toBe(true);
+const board = [
+    9, 0, 6,  3, 4, 0,  8, 1, 0, 
+    0, 5, 1,  7, 0, 0,  3, 0, 0, 
+    4, 7, 0,  0, 9, 1,  0, 0, 5, 
+    
+    0, 0, 0,  9, 0, 3,  0, 0, 2, 
+    0, 0, 2,  0, 8, 7,  0, 0, 0, 
+    1, 0, 7,  2, 0, 0,  6, 0, 0, 
+    
+    0, 8, 5,  0, 0, 9,  1, 0, 0, 
+    0, 3, 4,  0, 6, 0,  0, 0, 9, 
+    0, 1, 0,  5, 0, 8,  7, 0, 6, 
+];
+
+describe('given a non empty square', () => {
+    const x = 0;
+    const y = 0;
+    let result;
+    describe('when evaluating', () => {
+        beforeEach(() => {
+            result = Engine.evaluate(board, x, y);
+        });
+
+        it('stays the same', () => {
+            expect(result).toBe(9);
+        });
     });
 });
