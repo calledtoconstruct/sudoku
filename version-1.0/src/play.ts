ea4199108de54c75d47f9de5d4ad2cb59939e0b6
Joseph Woolley <joe@calledtoconstruct.net>
2018-11-26 07:40:38 -0500

    Refactoring; cosmetic changes to output.

--- version-1.0/src/play.ts
+++ version-1.0/src/play.ts
@@ -42,6 +42,20 @@ const expert: Array<number> = [
     0, 0, 7, 0, 0, 0, 0, 0, 9,
 ];
 
+const silly: Array<number> = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 1, 0, 0, 0, 0, 0,
+
+    0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 1, 0,
+    0, 0, 0, 0, 0, 1, 0, 0, 0,
+
+    0, 0, 0, 9, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0,
+];
+
 class Guess {
     private option: number = 0;
     constructor(
@@ -51,7 +65,7 @@ class Guess {
     ) { }
 
     next(): boolean {
-        if (this.option < this.options.length) {
+        if (this.option < this.options.length - 1) {
             ++this.option;
             return true;
         } else {
@@ -60,7 +74,8 @@ class Guess {
     }
 
     apply(board: Array<number>): void {
-        console.log(`applying ${this.options[this.option]} to ${this.x}, ${this.y}`);
+        // tslint:disable-next-line:max-line-length
+        console.log(`guessing option ${this.option} of ${this.options.length} by applying ${this.options[this.option]} to ${this.x}, ${this.y}`);
         board[this.y * 9 + this.x] = this.options[this.option];
     }
 
@@ -78,35 +93,99 @@ class Unknown {
     ) { }
 }
 
-const board: Array<number> = expert;
+const board: Array<number> = silly;
 let attempt: number = 0;
 let remaining: number;
 let last: number;
 let guessing: boolean = false;
 let guess: Array<Guess> = new Array<Guess>();
+const unknown: Array<Unknown> = new Array<Unknown>();
+let invalid: boolean = false;
+
+type PlayHandler = (x: number, y: number, play: Result) => void;
+type StrategyMap = { [K: string]: PlayHandler };
+
+const handleValidPlay: PlayHandler = (x: number, y: number, play: Result): void => {
+    board[y * 9 + x] = play.value;
+    if (guessing) {
+        guess.push(new Guess(x, y, new Array<number>()));
+    }
+};
+
+const handleUnknownPlay: PlayHandler = (x: number, y: number, play: Result): void => {
+    unknown.push(new Unknown(x, y, play.options));
+};
+
+const handleInvalidPlay: PlayHandler = (x: number, y: number, play: Result): void => {
+    invalid = true;
+};
+
+const strategy: StrategyMap = {
+    "valid": handleValidPlay,
+    "unknown": handleUnknownPlay,
+    "invalid": handleInvalidPlay
+};
+
+type VerticalHandler = (y: number) => void;
+type HorizontalHandler = (x: number, y: number) => void;
+
+// tslint:disable-next-line:max-line-length
+const scan: (vertical: VerticalHandler, horizontal: HorizontalHandler) => void = (vertical: VerticalHandler, horizontal: HorizontalHandler): void => {
+    for (let y: number = 0; y < 9; ++y) {
+        for (let x: number = 0; x < 9; ++x) {
+            horizontal(x, y);
+        }
+        vertical(y);
+    }
+};
+
+let line: string = '';
+
+const verticalLog: VerticalHandler = (y: number): void => {
+    if (y === 0) {
+        console.log('+---------+---------+---------+');
+    } else if (y % 3 === 0) {
+        console.log('+---------+---------+---------+');
+    }
+    console.log(line);
+    line = '';
+    if (y === 8) {
+        console.log('+---------+---------+---------+');
+    }
+};
+
+const horizontalLog: HorizontalHandler = (x: number, y: number): void => {
+    if (x === 0) {
+        line += '|';
+    } else if (x % 3 === 0) {
+        line += '|';
+    }
+    line += ' ';
+    line += board[y * 9 + x];
+    line += ' ';
+    if (x === 8) {
+        line += '|';
+    }
+};
+
+const evaluate: HorizontalHandler = (x: number, y: number): void => {
+    const play: Result = Engine.evaluate(board, x, y);
+    if (strategy[play.result]) {
+        strategy[play.result](x, y, play);
+    }
+};
+
 do {
     ++attempt;
-    const unknown: Array<Unknown> = new Array<Unknown>();
-    let invalid: boolean = false;
-    console.log('---------');
-    for (let y: number = 0; y < 9; ++y) {
-        let line: string = '';
-        for (let x: number = 0; x < 9; ++x) {
-            const play: Result = Engine.evaluate(board, x, y);
-            if (play.result === 'valid') {
-                board[y * 9 + x] = play.value;
-                if (guessing) {
-                    guess.push(new Guess(x, y, new Array<number>()));
-                }
-            } else if (play.result === 'unknown') {
-                unknown.push(new Unknown(x, y, play.options));
-            } else if (play.result === 'invalid') {
-                invalid = true;
-            }
-            line += board[y * 9 + x];
+    unknown.splice(0, unknown.length);
+    invalid = false;
+    scan(
+        (y: number): void => verticalLog(y),
+        (x: number, y: number): void => {
+            evaluate(x, y);
+            horizontalLog(x, y);
         }
-        console.log(line);
-    }
+    );
 
     remaining = board.reduce((count: number, current: number): number => count + ((current === 0) ? 1 : 0), 0);
     console.log(`After ${attempt} attempts, ${remaining} remain.`);
@@ -128,12 +207,12 @@ do {
         }
     } else if (last && last === remaining) {
         const canGuess: boolean = unknown.length > 0;
-        unknown.sort((red: Unknown, green: Unknown): number => green.options.length - red.options.length);
         if (canGuess) {
-            console.log('guessing...');
-            guessing = true;
-            const current: Guess = new Guess(unknown[0].x, unknown[0].y, unknown[0].options);
+            unknown.sort((red: Unknown, green: Unknown): number => green.options.length - red.options.length);
+            const best: Unknown = unknown[0];
+            const current: Guess = new Guess(best.x, best.y, best.options);
             if (current.next()) {
+                guessing = true;
                 current.apply(board);
                 guess.push(current);
             }
