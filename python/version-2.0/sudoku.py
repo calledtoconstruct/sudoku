b426ff379a9fe18a26b6057c3511606a505209cd
Joseph Woolley <joe@calledtoconstruct.net>
2018-12-01 18:44:27 -0500

    Return only remaining value when scanning horizontally

--- python/version-2.0/sudoku.py
+++ python/version-2.0/sudoku.py
@@ -9,8 +9,22 @@ def get(board, x, y):
     width, height = size(board)
     return board[y * width + x]
 
+def row_contains(board, y):
+    width, height = size(board)
+    options = []
+    for x in range(1, width + 1):
+        options.append(x)
+    print(options)
+    for x in range(width):
+        value = get(board, x, y)
+        if value != 0:
+            options.remove(value)
+    return options
+
 def evaluate(board, x, y):
     value = get(board, x, y)
     if value != 0:
         return value
-    
+    row = row_contains(board, y)
+    if len(row) == 1:
+        return row[0]
